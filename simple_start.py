c5911177f735a0ae946847fabf6a195e6f145dab
1048155341@qq.com <1048155341@qq.com>
2024-10-30 09:57:17 +0800

    设置了Chrome的driver路径

--- simple_start.py
+++ simple_start.py
@@ -1,8 +1,15 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+driver_path = "D:\\ChromeDriver\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe"
+
+#设置 Service 来指定ChromeDriver 的路径
+service = Service(executable_path=driver_path)
 
 
 #去除webdriver一些特征
@@ -11,8 +18,11 @@ option.add_experimental_option('excludeSwitches', ['enable-automation'])
 option.add_argument("--disable-blink-features")
 option.add_argument("--disable-blink-features=AutomationControlled")
 option.add_argument("--headless")
+
+
 # 设置Chrome驱动
-driver = Chrome(options=option)
+#driver = Chrome(options=option)
+driver = webdriver.Chrome(service=service, options=option)
 
 # 打开天猫店铺页面
 url = 'https://shop59996859.taobao.com/?spm=a230r.7195193.1997079397.28.591551f0oWOTfE'
